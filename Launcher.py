893296d8dc0b7cd6f07aaac20b3ef576133539ff
SilantevDenis <d.silantjev2012@gmail.com>
2023-02-19 17:00:19 +0300

    66666666666654
    zzzzzzzzzzzzzzzzzzzzzzzxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx22222222222222222222xxxxxxxxxxxxxxxxxxxxxxx2222222xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxgt

--- Launcher.py
+++ Launcher.py
@@ -74,8 +74,8 @@ import hashlib
 import argparse
 
 
-RAM_HOLDER_AMOUNT_base = 736000000 # reserve 512Mb of Memory
-ramholder = bytearray(RAM_HOLDER_AMOUNT_base)
+# RAM_HOLDER_AMOUNT_base = 736000000 # reserve 512Mb of Memory
+# ramholder = bytearray(RAM_HOLDER_AMOUNT_base)
 
 def DownloadSingleIPFSFile(ipfsHash, timeout_=5, max_trials_=2):
     ## constants & parameters
